b46697f8910ddaaab38677604d1f0352c08f07db
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-06 05:32:29 +0200

    Added Pokemon to reducers

--- my-app/src/app/store.js
+++ my-app/src/app/store.js
@@ -1,8 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 
+// Or from '@reduxjs/toolkit/query/react'
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { pokemonApi } from './services/pokemon'
+
+
+
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
+
+    // Add the generated reducer as a specific top-level slice
+    [pokemonApi.reducerPath]: pokemonApi.reducer,
   },
+
+  // Adding the api middleware enables caching, invalidation, polling,
+  // and other useful features of `rtk-query`.
+  middleware: (getDefaultMiddleware) =>
+  getDefaultMiddleware().concat(pokemonApi.middleware),
+
 });
