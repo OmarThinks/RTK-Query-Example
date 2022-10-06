6a49d507a86ef90f2e77b94be5162ee98347844d
Omar Magdy <24939461+OmarThinks@users.noreply.github.com>
2022-10-06 05:40:37 +0200

    Changed app to include balbasaur

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,23 @@ import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
 
+import { useGetPokemonByNameQuery } from './services/pokemon';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 function App() {
   return (
     <div className="App">
@@ -54,5 +71,34 @@ function App() {
     </div>
   );
 }
+*/
+
+
+function App() {
+  // Using a query hook automatically fetches data and returns query values
+  const { data, error, isLoading } = useGetPokemonByNameQuery('bulbasaur')
+  // Individual hooks are also accessible under the generated endpoints:
+  // const { data, error, isLoading } = pokemonApi.endpoints.getPokemonByName.useQuery('bulbasaur')
+
+
+  return (
+    <div className="App">
+      {error ? (
+        <>Oh no, there was an error</>
+      ) : isLoading ? (
+        <>Loading...</>
+      ) : data ? (
+        <>
+          <h3>{data.species.name}</h3>
+          <img src={data.sprites.front_shiny} alt={data.species.name} />
+        </>
+      ) : null}
+    </div>
+  )
+}
+
+
+
+
 
 export default App;
